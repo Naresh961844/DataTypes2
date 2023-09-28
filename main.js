cd9a183ca88f4e89a31a424f9d7abcfbd0eea9bd
Naresh961844 <146118593+Naresh961844@users.noreply.github.com>
2023-09-28 22:58:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function stringNumber(){
+
+
+let name="Virat";
+let number=1234567890;
+    a=(name+" : "+number);
+     document.getElementById("virat1").textContent=a;
+
+    
+    
+}
+
+function BooleanTrueFalse(){
+     const isTodayThursday=true;
+     v=isTodayThursday;
+     document.getElementById("virat2").textContent=v;
+}
+
+ function change(){
+
+let myArray=["yestraday"]; 
+     k=myArray;
+     
+ document.getElementById("virat3").textContent=k;
+     
+ }
